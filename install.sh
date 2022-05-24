1b3b75c1d1cdc8a479b99bfd74d027821541c3d0
Tristan Fountain <tfount96@gmail.com>
2022-05-23 20:56:10 -0500

    Initial version of install script for external dependencies

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 mkdir External
 mkdir External/Dolphin-memory-engine
+mkdir External/pybind
+mkdir External/pybind/pybind11
 git clone https://github.com/aldelaro5/Dolphin-memory-engine.git External/Dolphin-memory-engine
+git clone https://github.com/pybind/pybind11.git External/pybind/pybind11
