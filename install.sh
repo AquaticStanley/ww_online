396a4359210404f7f1468da38407da72c5204f82
Tristan Fountain <tfount96@gmail.com>
2025-03-07 14:32:49 -0600

    Lock dolphin memory engine dependency to a specific commit to avoid introducing unnecessary Qt dependency

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ mkdir External/pybind
 mkdir External/pybind/pybind11
 git clone https://github.com/aldelaro5/Dolphin-memory-engine.git External/Dolphin-memory-engine
 git clone https://github.com/pybind/pybind11.git External/pybind/pybind11
+cd External/Dolphin-memory-engine && git checkout dad2875d9a52c06826825b685c1eddccccf50796 && cd -;
